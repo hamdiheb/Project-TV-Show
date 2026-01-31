7f3ca6255864397909cef5b957d2334d532e6a30
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2026-01-31 15:28:28 +0100

    Wrote a script that display all episodes

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+  document.body.setAttribute("style","background-color: #EDEDED;")
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const sectionofMovies = document.createElement("section");
+  rootElem.append(sectionofMovies);
+  sectionofMovies.setAttribute('style','display: flex; flex-wrap: wrap; justify-content: center')
+  let components = '';
+  const episodeListtoDisplay = episodeList.map(element => {
+    components+=`<article style="width: 100%; max-width: 20%; margin: 10px; display: flex; flex-direction: column; background-color: #FFF;">
+    <h3 style="padding: 40px 20px; border: 1px solid black; border-radius: 1rem; color: black; text-align:center">${element.name}- S${element.season.toString().padStart(2,"0")}S${element.number.toString().padStart(2,"0")}</h3>
+    <div style="display: flex; flex-direction: column; align-items: center; width: 100%;">
+    <img style="width: 80%" src="${element.image.medium }"/>
+    ${element.summary}
+    </div>
+    </article>`;
+    }
+    );
+    sectionofMovies.innerHTML = components;
+
+    const paragraph = sectionofMovies.querySelectorAll("p");
+    paragraph.forEach(element => {
+        element.setAttribute("style","text-align: start; width: 81%; color: grey");
+    });
+
+  // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
 window.onload = setup;
