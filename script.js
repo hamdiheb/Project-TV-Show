73a6dd47acdad67305e24be8062848a516fc943b
Iheb Hamdi <67348203+hamdiheb@users.noreply.github.com>
2026-02-04 11:58:03 +0100

    Added to the search function the option to display movies which their summary any word match the input search value

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function setup() {
   searchButton.addEventListener("click", () => {
    rootElem.innerHTML = `<div id="root"></div>`
    filteredMovies = allEpisodes.filter(element => {
-    if(element.name.toUpperCase().includes(searchInput.value.toUpperCase())){
+    if(element.name.toUpperCase().includes(searchInput.value.toUpperCase()) || element.summary.toUpperCase().includes(searchInput.value.toUpperCase())){
       movieCounter++;
       return element
     }
@@ -24,34 +24,6 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
-  // First Option
-  // const rootElem = document.getElementById("root");
-  // const sectionofMovies = document.createElement("section");
-  // rootElem.append(sectionofMovies);
-  // sectionofMovies.setAttribute('style','display: flex; flex-wrap: wrap; justify-content: center')
-  // let components = '';
-  // const episodeListtoDisplay = episodeList.map(element => {
-  //   components+=`<article style="width: 100%; max-width: 20%; margin: 10px; display: flex; flex-direction: column; background-color: #FFF;">
-  //   <h3 style="padding: 40px 20px; border: 1px solid black; border-radius: 1rem; color: black; text-align:center">${element.name}- S${element.season.toString().padStart(2,"0")}S${element.number.toString().padStart(2,"0")}</h3>
-  //   <div style="display: flex; flex-direction: column; align-items: center; width: 100%;">
-  //   <img style="width: 80%" src="${element.image.medium }"/>
-  //   ${element.summary}
-  //   </div>
-  //   </article>`;
-  //   }
-  //   );
-  //   sectionofMovies.innerHTML = components;
-
-  //   const paragraph = sectionofMovies.querySelectorAll("p");
-  //   paragraph.forEach(element => {
-  //       element.setAttribute("style","text-align: start; width: 81%; color: grey");
-  //   });
-
-  // // rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-
-
-  // Second Option with function and destructing
-  // const rootElem = document.getElementById("root");
   const sectionMovies = document.createElement("section");
   rootElem.append(sectionMovies);
   const component = displayMovies(episodeList,sectionMovies);
