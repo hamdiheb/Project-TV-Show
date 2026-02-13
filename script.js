9c22e7b3767352b9e4b6f946949ce2cac08bbf1e
Roman <roman.boxes@gmail.com>
2026-02-13 12:38:15 +0100

    fix(template - summary, runTime and rating), refactor  function renderAll inside render function,  prettier
    fix(template - summary, runTime and rating)
    - now the template has correct summary, different for every episode.
    - the runTime and rating is from the allEpisodes object
    - remove the basic content from template, now it is only a structure
     function renderAll() is the independent function and don't see the reason for defining it inside the render function

--- script.js
+++ script.js
@@ -1,83 +1,92 @@
-function render(allEpisodes){
-    function renderAll(){
-        allEpisodes.map(element => {
-        document.querySelector(".episodes-display").append(episodeComponent(element));
-    })};
-    
-    renderAll();
+    function renderAll(allEpisodes){
+            allEpisodes.map(element => {
+            document.querySelector(".episodes-display").append(episodeComponent(element));
+            })
+    };
+        
+    function render(allEpisodes) {
+        
+        console.log(allEpisodes)
+        renderAll(allEpisodes);
 
-    const inputSearch = document.querySelector("#search-input");
-    inputSearch.addEventListener("keyup", () =>{
-        filterEpisodes(allEpisodes,inputSearch.value);
-    }); 
+        const inputSearch = document.querySelector("#search-input");
+        inputSearch.addEventListener("keyup", () =>{
+            filterEpisodes(allEpisodes,inputSearch.value);
+        }); 
 
-    const select = document.querySelector("select");
-    const episodesList = allEpisodes.map(element => {
-        const optionClone = document.querySelector(".season-select-option").cloneNode(true);
-        optionClone.innerText = `${element.name}`;
-        select.append(optionClone);
-    });
+        const select = document.querySelector("select");
+        const episodesList = allEpisodes.map(element => {
+            const optionClone = document.querySelector(".season-select-option").cloneNode(true);
+            optionClone.innerText = `${element.name}`;
+            select.append(optionClone);
+        });
 
-    select.addEventListener("change", (event) => {
-        const selectedOption = event.target.value;
-        selectedOption!='All Episodes' ? filterEpisodes(allEpisodes,selectedOption) : renderAll();
-    });
-}
-
-function episodeComponent(element){
-    const {name, season, number, summary} = element;
-    const {medium} = element.image;
-    const componentCloned = document.querySelector("template").cloneNode(true);
-    componentCloned.content.querySelector("h5").innerText = `S${season.toString().padStart(2,'0')}E${number.toString().padStart(2,'0')}`;
-    componentCloned.content.querySelector("img").src = medium;
-    componentCloned.content.querySelector("h3").innerText = `${name}`;
-    componentCloned.content.querySelector("p").innerHtml = summary;
-    const newArticle = document.createElement("div");
-    newArticle.classList.add("episode-component");
-    newArticle.append(componentCloned.content);
-    return newArticle;
-}
-
-function filterEpisodes(allEpisodes,input){
-    document.querySelector(".episodes-display").innerHTML = `
-                    <template class="episode-component">
-                    <h5 class="episode-nb-sn">S01E01</h5>
-                    <img class="episode-img" src="http://static.tvmaze.com/uploads/images/medium_landscape/1/2668.jpg" alt="episode-image"/>
-                    <h3 class="episode-title margin">Winter is Coming</h3>
-                    <p class="episode-summary margin">Lord Eddard Stark, ruler of the North, is summoned to court by his old friend, King Robert Baratheon, to serve as the King's Hand. Eddard reluctantly agrees after learning of a possible threat to the King's life. Eddard's bastard son Jon Snow must make a painful decision about his own future, while in the distant east Viserys Targaryen plots to reclaim his father's throne, usurped by Robert, by selling his sister in marriage.</p>
-                    <div class="episode-duration-rate">
-                        <p>62 min</p>
-                        <p>8.91</p>
-                    </div>
-                </template>
-    `;
-    let count =0;
-    allEpisodes.filter(element => {
-        if((element.name.toUpperCase().includes(input.toUpperCase())) || (element.summary.toUpperCase().includes(input.toUpperCase()))){
-            const newcomponent = episodeComponent(element);
-            document.querySelector(".episodes-display").append(newcomponent);
-            count++;
-            document.querySelector(".episode-numbers") .innerText = `Displaying ${count}/73`;
-        }
-    });
-}
-
-async function apiFetch(){
-    const render = document.querySelector(".rendering");
-    const res = await fetch("https://api.tvmaze.com/shows/22/episodes");
-    if(res.ok){
-        const data = await res.json();
-        render.remove()
-        return data;
-    }else{
-        alert("API data couldn't be rendered Error");
-        render.innerText = `API data couldn't be rendered Error ${res.status}`;
+        select.addEventListener("change", (event) => {
+            const selectedOption = event.target.value;
+            selectedOption!='All Episodes' ? filterEpisodes(allEpisodes,selectedOption) : renderAll(allEpisodes);
+        });
     }
-}
 
-async function main(){
-    const allEpisodes=await apiFetch();
-    render(allEpisodes);
-}
+    function episodeComponent(element){
+        const {name, season, number, summary,rating,runtime} = element;
+        const {medium} = element.image;
+        const componentCloned = document.querySelector("template").cloneNode(true);
+        componentCloned.content.querySelector("h5").innerText = `S${season.toString().padStart(2,'0')}E${number.toString().padStart(2,'0')}`;
+        componentCloned.content.querySelector("img").src = medium;
+        componentCloned.content.querySelector("h3").innerText = `${name}`;
+        componentCloned.content.querySelector(".episode-summary").innerHTML = `${summary}`;
+        const durationRateContainer = componentCloned.content.querySelector(".episode-duration-rate");
+        const time = durationRateContainer.querySelector("#runTime");
+        const rank = durationRateContainer.querySelector("#rating");
+        time.innerText = `Duration: ${runtime} min`;
+        rank.innerText = `Rating: ${rating.average.toFixed(1)}`;
+        
+        const newArticle = document.createElement("div");
+        newArticle.classList.add("episode-component");
+        newArticle.append(componentCloned.content);
+        return newArticle;
+    }
 
-main();
+    function filterEpisodes(allEpisodes,input){
+        document.querySelector(".episodes-display").innerHTML = `
+                        <template class="episode-component">
+                        <h5 class="episode-nb-sn"></h5>
+                        <img class="episode-img"/>
+                        <h3 class="episode-title margin"></h3>
+                        <p class="episode-summary"></p>
+                        <div class="episode-duration-rate">
+                            <p id="runTime"></p>
+                            <p id="rating"></p>
+                        </div>
+                    </template>
+        `;
+        let count = 0;
+        allEpisodes.filter(element => {
+            if((element.name.toUpperCase().includes(input.toUpperCase())) || (element.summary.toUpperCase().includes(input.toUpperCase()))){
+                const newComponent = episodeComponent(element);
+                document.querySelector(".episodes-display").append(newComponent);
+                count++;
+                document.querySelector(".episode-numbers") .innerText = `Displaying ${count}/73`;
+            }
+        });
+    }
+
+    async function apiFetch(){
+        const render = document.querySelector(".rendering");
+        const res = await fetch("https://api.tvmaze.com/shows/22/episodes");
+        if(res.ok){
+            const data = await res.json();
+            render.remove()
+            return data;
+        }else{
+            alert("API data couldn't be rendered Error");
+            render.innerText = `API data couldn't be rendered Error ${res.status}`;
+        }
+    }
+
+    async function main(){
+        const allEpisodes=await apiFetch();
+        render(allEpisodes);
+    }
+
+    main();
